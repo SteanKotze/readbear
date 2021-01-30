e64a45658479b82cb90c2c642f459c94546fd608
Stean Kotze <steankotze@gmail.com>
2021-01-30 17:41:03 +0200

    [Frontend] Done

--- question_2/src/App.js
+++ question_2/src/App.js
@@ -1,19 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import './custom.css';
+import mobile_svg from './mobile.svg';
 
 function App() {
   return (
-    <div class="App">
-      <nav class="md:flex pt-6 px-12">
+    <div class="App flex flex-col min-h-screen">
+      <nav class="md:flex pt-6 lg:px-20 shadow-rb_md_header">
         <div class="container flex justify-start pb-6">
-          <p class="text-rb_lg font-bold text-black">app.</p>
-          <p class="text-rb_lg font-bold text-rb_purple">me</p>
+          <a href="" class="text-48px font-bold text-black md:pl-20">app.</a>
+          <a href="" class="text-48px font-bold text-rb_purple">me</a>
         </div>
-        <div class="container md:flex md:justify-end sm:justify-start">
-          <div class="text-rb_md font-bold text-rb_purple md:pb-5 pt-1 w-133px md:text-center">Why</div>
-          <div class="text-rb_md font-bold text-rb_purple pb-5 pt-1 w-133px md:text-center">Contact</div>
+        <div class="container md:flex md:justify-end md:pr-20">
+          <a href="" class="text-26px font-bold text-rb_purple md:pb-5 pt-1 w-133px md:text-center">Why</a>
+          <a href="" class="text-26px font-bold text-rb_purple pb-5 pt-1 w-133px md:text-center">Contact</a>
         </div>
       </nav>
+      <div class="static body lg:flex flex-grow lg:px-20">
+        <div class="md:pl-20 lg:w-1/2">
+          <div class="fs-96 fw-700 pb-10">
+            the coolest new app...
+          </div>
+          <div class="fc-dark-gray fs-22 fw-700">
+            get notified when we go live
+          </div>
+          <div class="w-1/2 pt-5">
+            <input type="text" class="focus:outline-none fs-26 fw-500 pl-5 my-auto email-input" placeholder="your@cool.email"/>
+            <a class="email-button flex mt-5 fs-22 fw-700" href="">Join Waitlist</a>
+          </div>
+        </div>
+        <div class="lg:w-1/2 img-w700">
+          <img src={mobile_svg} alt="Mobile SVG"/>
+        </div>
+        <div class="absolute bg-rect"></div>
+      </div>
+      <div class="footer flex pt-6 lg:px-20 bg-rb_light_purple fs-22">
+        <div class="footer_text md:my-auto md:flex">
+          <div class="flex">
+            <p class="font-bold text-black md:pl-20">app.</p>
+            <p class="font-bold text-rb_purple md:pr-10">me</p>
+          </div>
+          <div class="md:flex">
+            <p class="footer_box fw-500 text-rb_gray">github</p>
+            <p class="footer_box fw-500 text-rb_gray">twitter</p>
+            <p class="footer_box fw-500 text-rb_gray">facebook</p>
+          </div>
+        </div>
+        <div class="footer_text md:my-auto container flex justify-end md:pr-20">
+          <p class="footer_box fw-500 text-rb_gray">privacy</p>
+          <p class="footer_box fw-500 text-rb_gray">terms</p>
+        </div>
+      </div>
     </div>
   );
 }
--- question_2/tailwind.config.js
+++ question_2/tailwind.config.js
@@ -1,18 +1,27 @@
 module.exports = {
   purge: ['./src/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {
+      boxShadow: {
+        rb_md_header: '0px 8px 4px 0px rgba(105, 48, 195, 0.06)',
+        rb_sm_input: '3px 3px 3px 3px rgba(105, 48, 195, 0.62)'
+      },
       colors: {
         rb_purple: {
-          light: '#6930C3',
-          DEFAULT: '#6930C3',
-          dark: '#6930C3',
+          DEFAULT: '#6930C3'
+        },
+        rb_light_purple: {
+          DEFAULT: 'rgba(105, 48, 195, 0.05)'
+        },
+        rb_gray: {
+          DEFAULT: '#7B7B7B'
         }
       },
       fontSize: {
-        rb_md: ['26px', '48px'],
-        rb_lg: ['48px', '48px']
+        '22px': ['22px', '22px'],
+        '26px': ['26px', '48px'],
+        '48px': ['48px', '48px']
       },
       spacing: {
         '133px': '8.313rem'
