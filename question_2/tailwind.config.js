276b5895e574b1e757e60531e7b183e0d3feb4da
Stean Kotze <steankotze@gmail.com>
2021-01-30 14:40:48 +0200

    [Frontend] Implemented navbar

--- question_2/tailwind.config.js
+++ question_2/tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: ['./src/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rb_purple: {
+          light: '#6930C3',
+          DEFAULT: '#6930C3',
+          dark: '#6930C3',
+        }
+      },
+      fontSize: {
+        rb_md: ['26px', '48px'],
+        rb_lg: ['48px', '48px']
+      },
+      spacing: {
+        '133px': '8.313rem'
+      }
+    }
   },
   variants: {
     extend: {},
